3f1395b05654a19183c26873b0bd7e06684c325e
Daniel Skinner <daniel@dasa.cc>
2014-09-02 12:14:11 -0500

    proper impl

--- htm.go
+++ htm.go
@@ -1,144 +1,116 @@
 package htm
 
 import (
-	"errors"
-	"fmt"
-
-	"azul3d.org/gfx.v1"
 	"azul3d.org/lmath.v1"
 )
 
+type Tree struct {
+	Name     string
+	Indices  [3]int
+	Vertices *[]lmath.Vec3
+
+	T0 *Tree
+	T1 *Tree
+	T2 *Tree
+	T3 *Tree
+}
+
+func NewTree(name string, verts *[]lmath.Vec3, i0, i1, i2 int) *Tree {
+	return &Tree{
+		Name:     name,
+		Indices:  [3]int{i0, i1, i2},
+		Vertices: verts,
+	}
+}
+
+func (t *Tree) SubDivide(level int) {
+	if len(t.Name) > level {
+		return
+	}
+
+	i0, i1, i2 := t.Indices[0], t.Indices[1], t.Indices[2]
+	v0, v1, v2 := (*t.Vertices)[i0], (*t.Vertices)[i1], (*t.Vertices)[i2]
+
+	w0, _ := v1.Add(v2).Normalized()
+	w1, _ := v0.Add(v2).Normalized()
+	w2, _ := v0.Add(v1).Normalized()
+
+	*t.Vertices = append(*t.Vertices, w0, w1, w2)
+
+	l := len(*t.Vertices)
+
+	t.T0 = NewTree(t.Name+"0", t.Vertices, i0, l-1, l-2)  // v0, w2, w1
+	t.T1 = NewTree(t.Name+"1", t.Vertices, i1, l-3, l-1)  // v1, w0, w2
+	t.T2 = NewTree(t.Name+"2", t.Vertices, i2, l-2, l-3)  // v2, w1, w0
+	t.T3 = NewTree(t.Name+"3", t.Vertices, l-3, l-2, l-1) // w0, w1, w2
+
+	t.T0.SubDivide(level)
+	t.T1.SubDivide(level)
+	t.T2.SubDivide(level)
+	t.T3.SubDivide(level)
+}
+
+func (t *Tree) CollectIndices(indices *[]uint32) {
+	if t.T0 == nil {
+		*indices = append(*indices, uint32(t.Indices[0]), uint32(t.Indices[1]), uint32(t.Indices[2]))
+	} else {
+		t.T0.CollectIndices(indices)
+		t.T1.CollectIndices(indices)
+		t.T2.CollectIndices(indices)
+		t.T3.CollectIndices(indices)
+	}
+}
+
 type HTM struct {
-	Vertices []lmath.Vec3
-	Indices  [][]int
-	Nodes    map[string]int
+	Vertices *[]lmath.Vec3
+
+	S0, S1, S2, S3 *Tree
+	N0, N1, N2, N3 *Tree
 }
 
-func NewHTM() *HTM {
+func New() *HTM {
+	verts := []lmath.Vec3{
+		{0, 0, 1},
+		{1, 0, 0},
+		{0, 1, 0},
+		{-1, 0, 0},
+		{0, -1, 0},
+		{0, 0, -1},
+	}
 	return &HTM{
-		Vertices: []lmath.Vec3{
-			{0, 0, 1},
-			{1, 0, 0},
-			{0, 1, 0},
-			{-1, 0, 0},
-			{0, -1, 0},
-			{0, 0, -1},
-		},
-		Indices: [][]int{
-			{1, 5, 2}, // S0
-			{2, 5, 3}, // S1
-			{3, 5, 4}, // S2
-			{4, 5, 1}, // S3
+		Vertices: &verts,
 
-			{1, 0, 4}, // N0
-			{4, 0, 3}, // N1
-			{3, 0, 2}, // N2
-			{2, 0, 1}, // N3
-		},
-		Nodes: map[string]int{
-			"S0": 0,
-			"S1": 1,
-			"S2": 2,
-			"S3": 3,
-			"N0": 4,
-			"N1": 5,
-			"N2": 6,
-			"N3": 7,
-		},
+		S0: NewTree("S0", &verts, 1, 5, 2),
+		S1: NewTree("S1", &verts, 2, 5, 3),
+		S2: NewTree("S2", &verts, 3, 5, 4),
+		S3: NewTree("S3", &verts, 4, 5, 1),
+		N0: NewTree("N0", &verts, 1, 0, 4),
+		N1: NewTree("N1", &verts, 4, 0, 3),
+		N2: NewTree("N2", &verts, 3, 0, 2),
+		N3: NewTree("N3", &verts, 2, 0, 1),
 	}
 }
 
-func (h *HTM) LookupNode(x string) ([]int, error) {
-	if i, ok := h.Nodes[x]; ok {
-		return h.Indices[i], nil
-	}
-	return nil, errors.New(fmt.Sprintf("Node name '%s' does not exist.", x))
+func (h *HTM) SubDivide(level int) {
+	h.S0.SubDivide(level)
+	h.S1.SubDivide(level)
+	h.S2.SubDivide(level)
+	h.S3.SubDivide(level)
+	h.N0.SubDivide(level)
+	h.N1.SubDivide(level)
+	h.N2.SubDivide(level)
+	h.N3.SubDivide(level)
 }
 
-func (h *HTM) ConnectMidpoints(i0, i1 int) lmath.Vec3 {
-	v0, v1 := h.Vertices[i0], h.Vertices[i1]
-	w0, _ := v0.Add(v1).Normalized()
-	return w0
-}
-
-func (h *HTM) IterLevels(x int) error {
-	for i := 2; i <= x; i++ {
-		for k := range h.Nodes {
-			if len(k) == i {
-				err := h.NextLevel(k)
-				if err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func (h *HTM) NextLevel(x string) error {
-	n, err := h.LookupNode(x)
-	if err != nil {
-		return err
-	}
-
-	v0, v1, v2 := n[0], n[1], n[2]
-
-	w0 := h.ConnectMidpoints(v1, v2)
-	w1 := h.ConnectMidpoints(v0, v2)
-	w2 := h.ConnectMidpoints(v0, v1)
-
-	h.Vertices = append(h.Vertices, w0, w1, w2)
-
-	l := len(h.Vertices)
-	ind := [][]int{
-		{v0, l - 1, l - 2},    // v0, w2, w1
-		{v1, l - 3, l - 1},    // v1, w0, w2
-		{v2, l - 2, l - 3},    // v2, w1, w0
-		{l - 3, l - 2, l - 1}, // w0, w1, w2
-	}
-
-	h.Nodes[x+"0"] = len(h.Indices)
-	h.Nodes[x+"1"] = len(h.Indices) + 1
-	h.Nodes[x+"2"] = len(h.Indices) + 2
-	h.Nodes[x+"3"] = len(h.Indices) + 3
-
-	h.Indices = append(h.Indices, ind...)
-
-	return nil
-}
-
-func (h *HTM) ConvertVertices() []gfx.Vec3 {
-	var vertices []gfx.Vec3
-	for _, a := range h.Vertices {
-		vertices = append(vertices, gfx.ConvertVec3(a))
-	}
-	return vertices
-}
-
-func (h *HTM) ConvertIndices() []uint32 {
+func (h *HTM) Indices() []uint32 {
 	var indices []uint32
-	for _, a := range h.Indices {
-		for _, b := range a {
-			indices = append(indices, uint32(b))
-		}
-	}
+	h.S0.CollectIndices(&indices)
+	h.S1.CollectIndices(&indices)
+	h.S2.CollectIndices(&indices)
+	h.S3.CollectIndices(&indices)
+	h.N0.CollectIndices(&indices)
+	h.N1.CollectIndices(&indices)
+	h.N2.CollectIndices(&indices)
+	h.N3.CollectIndices(&indices)
 	return indices
-}
-
-func (h *HTM) NewObject() *gfx.Object {
-	o := gfx.NewObject()
-	m := gfx.NewMesh()
-	m.Vertices = h.ConvertVertices()
-	m.Indices = h.ConvertIndices()
-	o.Meshes = []*gfx.Mesh{m}
-	return o
-}
-
-func (h *HTM) PPrint() {
-	for _, v := range h.Vertices {
-		fmt.Println(v)
-	}
-	for _, n := range h.Indices {
-		fmt.Println(n)
-	}
 }
