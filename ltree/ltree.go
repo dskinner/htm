19ffc821f0b98536134fc46fce34b57fa915011f
Daniel Skinner <daniel@dasa.cc>
2014-11-01 19:30:54 -0500

    remove vec types, uint64, additional column-major checks

--- ltree/ltree.go
+++ ltree/ltree.go
@@ -1,16 +1,10 @@
 // Package ltree provides parallelizable functions for a linear quad tree.
 package ltree
 
-type Vec2 struct {
-	X, Y uint32
-}
-
-type Vec2f struct {
-	X, Y float32
-}
+import "math"
 
 // Undilate deinterleaves word using shift-or algorithm.
-func Undilate(x uint32) uint32 {
+func Undilate(x uint64) uint64 {
 	x = (x | (x >> 1)) & 0x33333333
 	x = (x | (x >> 2)) & 0x0F0F0F0F
 	x = (x | (x >> 4)) & 0x00FF00FF
@@ -19,39 +13,102 @@ func Undilate(x uint32) uint32 {
 }
 
 // Decode retrieves column major position and level from word.
-func Decode(key uint32) (level uint32, p Vec2) {
+func Decode(key uint64) (x, y, level uint64) {
+	x = Undilate((key >> 4) & 0x05555555)
+	y = Undilate((key >> 5) & 0x55555555)
 	level = key & 0xF
-	p.X = Undilate((key >> 4) & 0x05555555)
-	p.Y = Undilate((key >> 5) & 0x55555555)
 	return
 }
 
 // Children generates nodes from a quadtree encoded word.
-func Children(key uint32) [4]uint32 {
-	var c [4]uint32
+func Children(key uint64) (uint64, uint64, uint64, uint64) {
 	key = ((key + 1) & 0xF) | ((key & 0xFFFFFFF0) << 2)
-	c[0] = key
-	c[1] = key | 0x10
-	c[2] = key | 0x20
-	c[3] = key | 0x30
-	return c
+	return key, key | 0x10, key | 0x20, key | 0x30
 }
 
 // Parent generates node from quadtree encoded word.
-func Parent(key uint32) uint32 {
+func Parent(key uint64) uint64 {
 	return ((key - 1) & 0xF) | ((key >> 2) & 0x3FFFFFF0)
 }
 
 // IsUpperLeft determines if node represents the upper-left child of its parent.
-func IsUpperLeft(key uint32) bool {
+func IsUpperLeft(key uint64) bool {
 	return ((key & 0x30) == 0x00)
 }
 
+// IsUpperRight determines if node represents the upper-right child of its parent.
+func IsUpperRight(key uint64) bool {
+	return ((key & 0x30) == 0x10)
+}
+
+// IsLowerLeft determines if node represents the lower-left child of its parent.
+func IsLowerLeft(key uint64) bool {
+	return ((key & 0x30) == 0x20)
+}
+
+// IsLowerRight determines if node represents the lower-right child of its parent.
+func IsLowerRight(key uint64) bool {
+	return ((key & 0x30) == 0x30)
+}
+
 // Cell retrieves normalized coordinates and size.
-func Cell(key uint32) (p Vec2f, size float32) {
-	level, pos := Decode(key)
-	size = 1 / float32(uint32(1<<level))
-	p.X = float32(pos.X) * size
-	p.Y = float32(pos.Y) * size
+// TODO(d) want more types of cells. Decode returns either (0, 0) (1, 0) (0, 1) (1, 1)
+// and then size is used to scale that down correctly which is based on level. That's
+// fine for a grid, but how might I infer other types from this, such as octahedron,
+// or just northern/southern part. In the current case, x and y are used to represent
+// vertices, but they might instead be used to represent faces since each pair is unique
+// which is simple enough, but then the key level would somehow need to infer the rest
+// of the information to produce 3D coordinates. That would mean Decode would return
+// a unique face and then level would need to be able to infer a set of three vertices.
+// A seperate data structure could manage mesh data and topology for effecient rendering
+// and subdivision methods for arbitrary/noisey meshes, but funcs such as this could be
+// used to generate initial primitives are possibly even reproduce algorithmic meshes in
+// a localized manner.
+//
+// Example:
+// f0: (0, 0) i...: 1, 0, 4
+// f1: (1, 0) i...: 4, 0, 3
+// f2: (1, 1) i...: 3, 0, 2
+// f3: (0, 1) i...: 2, 0, 1
+//
+// v0: {0, 0, 1},
+// v1: {1, 0, 0},
+// v2: {0, 1, 0},
+// v3: {-1, 0, 0},
+// v4: {0, -1, 0},
+//
+func Cell(key uint64) (nx, ny, size float64) {
+	x, y, level := Decode(key)
+	size = 1 / float64(uint64(1<<level))
+	nx = float64(x) * size
+	ny = float64(y) * size
+	return
+}
+
+// Cap calculates the required capacity to hold all nodes of a given level.
+func Cap(lvl int) int {
+	return int(math.Pow(4, float64(lvl)))
+}
+
+// Split recursively collects children at the given level into nodes pointer.
+func Split(key uint64, lvl int, nodes *[]uint64) {
+	if key&0xF == uint64(lvl) {
+		*nodes = append(*nodes, key)
+	} else {
+		a, b, c, d := Children(key)
+		Split(a, lvl, nodes)
+		Split(b, lvl, nodes)
+		Split(c, lvl, nodes)
+		Split(d, lvl, nodes)
+	}
+}
+
+// ProjectMercator converts normalized coordinates to mercator projection, just for fun.
+func ProjectMercator(nx, ny float64, radius float64) (x, y, z float64) {
+	nx = math.Pi / 4 * (2*nx - 1)
+	ny = math.Pi / 4 * (4*ny + 1)
+	x = radius * math.Cos(ny) * math.Cos(nx)
+	y = radius * math.Cos(ny) * math.Sin(nx)
+	z = radius * math.Sin(ny)
 	return
 }
--- ltree/ltree_test.go
+++ ltree/ltree_test.go
@@ -2,87 +2,74 @@ package ltree
 
 import (
 	"fmt"
-	"math"
 	"testing"
 )
 
-func printWord(t *testing.T, pad string, title string, key uint32) {
-	level, p := Decode(key)
-	norm, size := Cell(key)
+func printWord(t *testing.T, pad string, title string, key uint64) {
+	x, y, level := Decode(key)
+	nx, ny, size := Cell(key)
 	t.Logf("%s%s:\n", pad, title)
 	t.Logf("%s  parent: %b\n", pad, Parent(key))
 	t.Logf("%s  node: %b\n", pad, key)
 	t.Logf("%s  level: %v\n", pad, level)
-	t.Logf("%s  pos: %+v\n", pad, p)
-	t.Logf("%s  norm: %+v\n", pad, norm)
+	t.Logf("%s  pos: %v, %v\n", pad, x, y)
+	t.Logf("%s  norm: %v, %v\n", pad, nx, ny)
 	t.Logf("%s  size: %v\n", pad, size)
 	t.Logf("%s  UL: %v\n", pad, IsUpperLeft(key))
 }
 
-func recurse(t *testing.T, parent uint32) {
-	for i, child := range Children(parent) {
-		plvl, _ := Decode(parent)
-		clvl, _ := Decode(child)
-		if plvl+1 != clvl {
-			t.Fail()
-		}
-		if Parent(child) != parent {
-			t.Fail()
-		}
-		if IsUpperLeft(child) && i != 0 {
-			t.Fail()
-		}
-		if clvl < 10 {
-			recurse(t, child)
-		}
-	}
-}
-
-func recurseAppend(parent uint32, nodes *[]uint32, lvl int, maxLvl int) {
-	clvl := lvl + 1
-	for _, child := range Children(parent) {
-		if clvl < maxLvl {
-			recurseAppend(child, nodes, clvl, maxLvl)
-		} else {
-			*nodes = append(*nodes, child)
-		}
-	}
-}
-
 func TestTree(t *testing.T) {
-	var root uint32 = 0
-
-	rlvl, _ := Decode(root)
-	if rlvl != 0 {
+	var root uint64 = 0
+	_, _, lvl := Decode(root)
+	if lvl != 0 {
 		t.Fail()
 	}
-
-	recurse(t, root)
 }
 
 func TestLength(t *testing.T) {
-	maxLvl := 10
-	maxLen := int(math.Pow(4, float64(maxLvl)))
-
-	var nodes []uint32
-	recurseAppend(0, &nodes, 0, maxLvl)
-	if len(nodes) != maxLen {
-		t.Fatalf("Expected %v but got %v\n", maxLen, len(nodes))
+	var nodes []uint64
+	lvl := 10
+	Split(0, lvl, &nodes)
+	if len(nodes) != Cap(lvl) {
+		t.Fatalf("Expected %v but got %v\n", Cap(lvl), len(nodes))
 	}
 }
 
 func TestPrint(t *testing.T) {
-	var x uint32 = 0
+	var x uint64 = 0
 	// 1110 0010
 	// x |= 1 << 1
 	// x |= 1 << 5
 	// x |= 1 << 6
 	// x |= 1 << 7
 
-	for i, a := range Children(x) {
-		printWord(t, "  ", fmt.Sprintf("x i %v", i), a)
-		for j, b := range Children(a) {
-			printWord(t, "    ", fmt.Sprintf("x i,j %v,%v", i, j), b)
+	var nodes []uint64
+	Split(x, 1, &nodes)
+	for i, n := range nodes {
+		printWord(t, "  ", fmt.Sprintf("%v", i), n)
+	}
+}
+
+func BenchmarkSplit11(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		nodes := make([]uint64, 0, Cap(11))
+		Split(0, 11, &nodes)
+		for _, key := range nodes {
+			nx, ny, _ := Cell(key)
+			_, _, _ = ProjectMercator(nx, ny, 1)
 		}
+	}
+}
+
+func BenchmarkChildren(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, _, _, _ = Children(0)
+	}
+}
+
+func BenchmarkParent(b *testing.B) {
+	a, _, _, _ := Children(0)
+	for n := 0; n < b.N; n++ {
+		_ = Parent(a)
 	}
 }
